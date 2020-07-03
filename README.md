3cb3ce5b8baf53360cc48fe3c700c56c67da19f7
Ítalo Vinícius <italovinicius18@yahoo.com>
2020-07-03 16:38:20 -0300

    Updating the path and verifying images sizes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Download-spotlight-Windows-10-wallpapers
+# Download spotlight Windows 10 wallpapers
 Simple Py3 script to save the spotlight wallpapers of Windows 10
+
+This script it will create a folder of spotlight wallpapers inside the cloned folder
+
+## Instructions
+
+Open Powershell and write the follow command:
+
+``` powershell
+python3 .\save_spotlight.py
+```
---
+++ save_spotlight.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import struct
+import imghdr
+
+def main():
+    userprofile = input('Insert the user profile name: ')
+    path = 'C:/Users/'+userprofile+'/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets'
+    destinationFolder = './Spotlight_Wallpapers'
+    copyFilesAndCreateDestFolder(path,userprofile,destinationFolder)
+    renameFilesToJPG(destinationFolder)
+
+def copyFilesAndCreateDestFolder(path,userprofile,destinationFolder):
+    if not os.path.isdir(destinationFolder):
+        os.mkdir(destinationFolder)
+
+    for file in os.listdir(path):
+        source = path+'/'+file
+        if verifyWallpapers(source):
+            shutil.copy(source, destinationFolder)
+
+def renameFilesToJPG(destinationFolder):
+    for file in os.listdir(destinationFolder):
+        oldName = destinationFolder+'/'+file
+        newName = oldName+'.jpg'
+        os.rename(oldName,newName)
+
+def getImageSize(fname):
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height
+
+def verifyWallpapers(sourceImage):
+    
+    width,height = getImageSize(sourceImage)
+
+    if height>1000:
+        return True
+    
+    return False
+
+if __name__ == "__main__":
+    main()
